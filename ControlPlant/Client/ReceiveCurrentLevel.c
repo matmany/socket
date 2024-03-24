4132a430dea62fd261bee811bdb852ba128a6ebd
Manny <mateus.araujo@nepen.org.br>
2024-03-24 19:28:58 -0300

    feat(receive): usando funções no receive

--- ControlPlant/Client/ReceiveCurrentLevel.c
+++ ControlPlant/Client/ReceiveCurrentLevel.c
@@ -14,51 +14,59 @@
 #include <unistd.h>
 // port through which connection is to be made
 
-
 #define CONNECTION_PORT 4243
+
+void setUpSocket(int *socket_descriptor, in_addr_t add);
 
 int main()
 {
+
+    int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    int receive_buffer;
+    
+    //inet_addr("3.86.248.23");
+    setUpSocket(&socket_descriptor, INADDR_ANY);
+  
+    while (1)
+    {
+        recv(socket_descriptor, &receive_buffer, sizeof(receive_buffer), 0);
+
+        int received_number = ntohl(receive_buffer);
+
+        printf("Valor Recebido: %d\n", received_number);
+    }
+
+    close(socket_descriptor);
+    return 0;
+}
+
+void setUpSocket(int *socket_descriptor, in_addr_t add)
+{
     // structure to represent the address
     struct sockaddr_in server_address;
-    // Message to be sent to the server
-    //char *message = "This is a message from the client";
-    // storage buffer to receive messages
-    char recieve_buffer[100];
-    // create an Ipv4 and TCP socket
-    int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+
     // Check whether the socket is created successfully
-    if (socket_descriptor < 0)
+    if (*socket_descriptor < 0)
     {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
     int status = 0;
+    
     // initialize address structure for binding
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(CONNECTION_PORT);
+    
     // set address to any address available
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_addr.s_addr = add;
+
     server_address.sin_zero[8] = '\0';
+    
     // connect to the server
-    status = connect(socket_descriptor, (struct sockaddr *)&server_address, sizeof(server_address));
+    status = connect(*socket_descriptor, (struct sockaddr *)&server_address, sizeof(server_address));
     if (status < 0)
     {
         perror("Couldn't connect with the server");
         exit(EXIT_FAILURE);
     }
-
-    int receive_buffer;
-    while (1)
-    {
-        //read(socket_descriptor, recieve_buffer, 100);
-        recv(socket_descriptor, &receive_buffer, sizeof(receive_buffer), 0);
-
-        int received_number = ntohl(receive_buffer);
-
-        printf("Valor no Nivel Atual: %d\n", received_number);
-    }
-
-    close(socket_descriptor);
-    return 0;
 }
